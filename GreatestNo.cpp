e6cfcee3ad3fb89fbe5ce2f0d623e6a4a212ae0f
Shreya Bhavsar <113428188+Shreyab20@users.noreply.github.com>
2023-04-24 15:44:50 +0530

    First commit

---
+++ GreatestNo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter value of a : "<<endl;
+    cin>>a;
+    cout<<"Enter value of b : "<<endl;
+    cin>>b;
+    if(a>b){
+        cout<<"a is greater";
+    }
+    else{
+        cout<<"b is greater";
+    }
+    return 0;
+
+}
